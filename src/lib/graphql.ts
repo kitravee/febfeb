5c9f22d7ee8211f5a16e3a9d81f6060e8318105b
kitravee <kitravee.swk@gmail.com>
2022-05-15 14:40:51 +0700

    fix: add link account function

--- src/lib/graphql.ts
+++ src/lib/graphql.ts
@@ -1,7 +1,10 @@
 import { GraphQLClient } from 'graphql-request';
 
-import { ENV } from '@/config/environment';
-
 export const gqlClient = new GraphQLClient(
-  ENV.GRAPH_CMS_API_ENDPOINT as string,
+  process.env.GRAPH_CMS_API_ENDPOINT as string,
+  {
+    headers: {
+      Authorization: `Bearer ${process.env.GRAPH_CMS_TOKEN}`,
+    },
+  },
 );
